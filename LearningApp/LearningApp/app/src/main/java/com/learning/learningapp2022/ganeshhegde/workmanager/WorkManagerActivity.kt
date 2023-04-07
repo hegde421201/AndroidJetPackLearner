07272da58156ac677fd912c4e7f71d99749f4d05
hegde421201 <hegde421201@gmail.com>
2023-04-07 10:14:07 +0530

    workmanager
    workmanager

---
+++ LearningApp/LearningApp/app/src/main/java/com/learning/learningapp2022/ganeshhegde/workmanager/WorkManagerActivity.kt
@@ -0,0 +1,12 @@
+package com.learning.learningapp2022.ganeshhegde.workmanager
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.learning.learningapp2022.ganeshhegde.R
+
+class WorkManagerActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_work_manager)
+    }
+}
