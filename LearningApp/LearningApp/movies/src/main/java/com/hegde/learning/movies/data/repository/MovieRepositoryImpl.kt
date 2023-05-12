c0aeecee6cf54537d0803d33df2e848d86676e7a
hegde421201 <hegde421201@gmail.com>
2023-05-12 18:28:57 +0530

    update
    update

--- LearningApp/LearningApp/movies/src/main/java/com/hegde/learning/movies/data/repository/MovieRepositoryImpl.kt
+++ LearningApp/LearningApp/movies/src/main/java/com/hegde/learning/movies/data/repository/MovieRepositoryImpl.kt
@@ -3,8 +3,10 @@ package com.hegde.learning.movies.data.repository
 import com.hegde.learning.movies.data.model.movie.Movie
 import com.hegde.learning.movies.domain.repository.MovieRepository
 
-class MovieRepositoryImpl(private val movieRemoteDataSource:MovieRemoteDataSource,
-private val movieLocalDataSource:MovieLocalDataSource,private val movieCacheDataSource:MovieCacheDataSource) : MovieRepository {
+class MovieRepositoryImpl(
+    private val movieRemoteDataSource:MovieRemoteDataSource,
+    private val movieLocalDataSource:MovieLocalDataSource,
+    private val movieCacheDataSource:MovieCacheDataSource) : MovieRepository {
     override suspend fun getMovies(): List<Movie>? {
 
         return null
