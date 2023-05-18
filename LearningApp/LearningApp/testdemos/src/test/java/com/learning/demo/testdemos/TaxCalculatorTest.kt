080416edbf9fe386db8837ba3c090cc86e5ccb89
hegde421201 <hegde421201@gmail.com>
2023-05-18 18:50:40 +0530

    update
    update

---
+++ LearningApp/LearningApp/testdemos/src/test/java/com/learning/demo/testdemos/TaxCalculatorTest.kt
@@ -0,0 +1,29 @@
+package com.learning.demo.testdemos
+
+import com.google.common.truth.Truth.assertThat
+import org.junit.jupiter.api.BeforeEach
+import org.junit.jupiter.api.Test
+
+class TaxCalculatorTest {
+
+   private lateinit var tax1 : TaxCalculator
+
+    @BeforeEach
+    fun setup(){
+        tax1 = TaxCalculator()
+    }
+
+    @Test
+    fun calculateTax() {
+
+        val taxPaid = tax1.calculateTax(1200.0,3/100.0)
+        assertThat(taxPaid).isEqualTo(36.0)
+    }
+
+    @Test
+    fun calculateIncome() {
+        val income = tax1.calculateIncome(1200.0,3/100.0)
+        assertThat(income).isEqualTo(1164.0)
+
+    }
+}
