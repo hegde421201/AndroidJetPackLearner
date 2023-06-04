23ccdce2cb967654da35b65502c24f769643dab9
hegde421201 <hegde421201@gmail.com>
2023-06-04 17:53:19 +0530

    update
    update

--- LearningApp/LearningApp/movies/src/main/java/com/hegde/learning/movies/presentation/movie/MovieActivity.kt
+++ LearningApp/LearningApp/movies/src/main/java/com/hegde/learning/movies/presentation/movie/MovieActivity.kt
@@ -3,12 +3,17 @@ package com.hegde.learning.movies.presentation.movie
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import android.view.View
+import android.widget.LinearLayout
+import android.widget.Toast
 import androidx.databinding.DataBindingUtil
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.hegde.learning.movies.R
 import com.hegde.learning.movies.databinding.ActivityMovieBinding
 import com.hegde.learning.movies.presentation.di.Injector
+import okhttp3.internal.notify
 import javax.inject.Inject
 
 class MovieActivity : AppCompatActivity() {
@@ -20,6 +25,8 @@ class MovieActivity : AppCompatActivity() {
 
         private lateinit var binding: ActivityMovieBinding
 
+        private lateinit var adapter: MovieAdapter
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
@@ -29,9 +36,31 @@ class MovieActivity : AppCompatActivity() {
 
         movieViewModel= ViewModelProvider(this,factory)[MovieViewModel::class.java]
 
+        initRecyclerView()
+    }
+
+    private fun initRecyclerView(){
+        binding.movieRecyclerView.layoutManager = LinearLayoutManager(this)
+        adapter = MovieAdapter()
+        binding.movieRecyclerView.adapter = adapter
+        displayPopularMovies()
+    }
+
+    private fun displayPopularMovies() {
+        binding.progressBar.visibility = View.VISIBLE
         val responseLiveData = movieViewModel.getMovies()
         responseLiveData.observe(this, Observer {
-            Log.i("TAG",it.toString())
+            it?.let{
+                adapter.setList(it)
+                adapter.notifyDataSetChanged()
+                binding.progressBar.visibility = View.GONE
+            }
+
+            if(it == null)
+            {
+                binding.progressBar.visibility = View.GONE
+                Toast.makeText(applicationContext,"No data available",Toast.LENGTH_SHORT).show()
+            }
         })
     }
 }
